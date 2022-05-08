1c8e14abefd3992436f96a3f8adf2abdf092ec2a
Võ Văn Việt <88840717+Vovanviet@users.noreply.github.com>
2022-05-08 16:33:33 +0700

    update

--- src/main/java/model/WriteFile.java
+++ src/main/java/model/WriteFile.java
@@ -4,8 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import entity.Product;
 
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
@@ -15,31 +13,18 @@ import java.util.List;
 
 public class WriteFile {
 
-    public static List<Product> WriteFile(List<Product> product){
+    public static void WriteFile(List<Product> product){
         try {
-
-//            StringBuilder str = new StringBuilder();
-//            FileOutputStream outputStream = new FileOutputStream("product.json");
-//            for(Product product1: product){
-//                str.append(product1.toString());
-//            }
-//
-//            byte[] strToBytes = str.toString().getBytes();
-//            outputStream.write(strToBytes);
-//
-//            outputStream.close();
-//            System.out.println("Successfully wrote to the file");
             Gson gson=new GsonBuilder().setPrettyPrinting().create();
             Writer writer=Files.newBufferedWriter(Paths.get("product.json"), StandardCharsets.UTF_8);
             gson.toJson(product,writer);
             writer.close();
-            System.out.println("Success");
+            System.out.println("Write Success to Json");
 
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-        return null;
     }
 }
